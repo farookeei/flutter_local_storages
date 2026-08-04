d966e9267d85a781a3999dc5be50c37b24a814da
Farook Jamal <farookyj10@gmail.com>
2026-08-04 12:10:01 +0530

    feat: setup base architecture, benchmark UI, contract tests, and main app ID

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_local_storages"
+        applicationId = "com.example.flutter_local_storages.main"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
